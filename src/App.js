d461bf6dcd152679bc9697c2463604e86c95d09a
hp <zekariastamiru12@gmail.com>
2024-12-11 22:32:56 +0300

    fetching data and using useReducer method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const intialState = {
+  questions: [],
+  // 'Loading','active','error','finished'
+  status: "Loading",
+};
+function Reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payLoad, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("action not found");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(Reducer, intialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payLoad: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header className="app-header" />
+      <Main>
+        <p>1/15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
