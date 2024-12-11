40c3ba03221567e9069fb2294a8f4e8a067a492e
hp <zekariastamiru12@gmail.com>
2024-12-11 23:48:06 +0300

    handling status( 'Loading','active','error','finished')

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
+import StartScreen from "./StartScreen";
+import Loader from "./Loader";
+import Error from "./Error";
 import Main from "./Main";
 
 const intialState = {
@@ -18,7 +21,9 @@ function Reducer(state, action) {
   }
 }
 export default function App() {
-  const [state, dispatch] = useReducer(Reducer, intialState);
+  const [{ status }, dispatch] = useReducer(Reducer, intialState);
+
+  const numQuestions = questions.length;
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -29,8 +34,9 @@ export default function App() {
     <div className="app">
       <Header className="app-header" />
       <Main>
-        <p>1/15</p>
-        <p>Questions?</p>
+        {status === "Loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestion} />}
       </Main>
     </div>
   );
---
+++ src/StartScreen.js
@@ -0,0 +1,10 @@
+function StartScreen() {
+  return (
+    <div className="start">
+      <h2>WELLCOME TO REACT QUESTIONS</h2>
+      <h3>15 questions to test your react mastery</h3>
+      <button className="btn btn-UI">Let's Start</button>
+    </div>
+  );
+}
+export default StartScreen;
