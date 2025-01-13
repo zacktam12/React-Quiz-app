e967be4807b7f6dd9c43c3f4a2915d02698f9501
hp <zekariastamiru12@gmail.com>
2025-01-13 16:04:08 +0300

    Refactor StartScreen to use QuizContext for state management

--- src/Components/StartScreen.js
+++ src/Components/StartScreen.js
@@ -1,4 +1,7 @@
-function StartScreen({ numQuestions, dispatch }) {
+import { useQuiz } from "../Context/QuizContext";
+
+function StartScreen() {
+  const { numQuestions, dispatch } = useQuiz();
   return (
     <div className="start">
       <h2>Welcome to React quize</h2>
